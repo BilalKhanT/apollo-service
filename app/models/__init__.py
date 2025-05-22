91cc19ce3ecf5ff9384f82867623728575a5238a
BilalKhanT <m.bilalkhant@gmail.com>
2025-05-22 18:02:30 +0500

    added db config

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,6 +3,7 @@ from .crawl_model import CrawlRequest, CrawlStatus
 from .scrape_model import ScrapingRequest, ScrapingStatus
 from .cluster_model import Cluster, DomainCluster, YearCluster, ClusterResult, ClusterDetailResponse, YearDetailResponse
 from .log_model import LogEntry, LogResponse 
+from .database.database_models import CrawlSummary, ClusterSummary, ClusterDocument, Cluster, ClusterResponse, CrawlResult, DomainCluster, YearDocument, YearExtractionSummary, YearResponse, ProcessSummary
 
 __all__ = [
     'CrawlRequest',
@@ -17,4 +18,15 @@ __all__ = [
     'LogResponse',
     'ClusterDetailResponse',
     'YearDetailResponse',
+    'CrawlSummary', 
+    'ProcessSummary',
+    'ClusterSummary', 
+    'YearExtractionSummary',
+    'Cluster', 
+    'DomainCluster',
+    'CrawlResult', 
+    'ClusterDocument', 
+    'YearDocument',
+    'ClusterResponse', 
+    'YearResponse',
 ]
