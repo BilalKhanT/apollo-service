50312cecb8479c1939ef19094de84c81b6cec776
BilalKhanT <m.bilalkhant@gmail.com>
2025-05-23 11:36:57 +0500

    .

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,6 +3,19 @@ from .crawl_model import CrawlRequest, CrawlStatus
 from .scrape_model import ScrapingRequest, ScrapingStatus
 from .cluster_model import Cluster, DomainCluster, YearCluster, ClusterResult, ClusterDetailResponse, YearDetailResponse
 from .log_model import LogEntry, LogResponse 
+# from .database.database_models import (
+#     CrawlResult,
+#     ClusterDocument,
+#     YearDocument,
+#     CrawlData,
+#     ProcessedLinks,
+#     CrawlSummary,
+#     ProcessSummary,
+#     ClusterSummary,
+#     YearExtractionSummary,
+#     Cluster as DBCluster,  # Rename to avoid conflict with cluster_model.Cluster
+#     DomainCluster as DBDomainCluster  # Rename to avoid conflict with cluster_model.DomainCluster
+# )
 
 __all__ = [
     'CrawlRequest',
@@ -17,4 +30,15 @@ __all__ = [
     'LogResponse',
     'ClusterDetailResponse',
     'YearDetailResponse',
+    'CrawlResult',
+    'ClusterDocument',
+    'YearDocument',
+    'CrawlData',
+    'ProcessedLinks',
+    'CrawlSummary',
+    'ProcessSummary',
+    'ClusterSummary',
+    'YearExtractionSummary',
+    'DBCluster',
+    'DBDomainCluster',
 ]
